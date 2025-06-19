86b4435bfd31bc2b09e179a87dc6cb1af223ca83
Im-Sena <sena.saeki@gmail.com>
2025-06-19 20:05:53 +0900

    日本中国韓国国籍の人の画像を集められるようにした

--- get_face.py
+++ get_face.py
@@ -1,5 +1,21 @@
-from icrawler.builtin import BingImageCrawler #icrawlerのインポート
+from icrawler.builtin import GoogleImageCrawler
+import datetime
 
-searchWord = '日本 芸能人 顔写真' #検索ワードの設定
-crawler = BingImageCrawler(storage = {'root_dir' : './image'})
-crawler.crawl(keyword = searchWord, max_num = 500)
+n_total_images = 100
+n_per_crawl = 100
+
+delta = datetime.timedelta(days=720)
+end_day = datetime.datetime(2023, 1, 29)
+
+def datetime2tuple(date):
+    return (date.year, date.month, date.day)
+
+keywords = ['japanese actor','korean actor','chinese actor']
+
+for keyword in keywords:
+    for i in range(int(n_total_images / n_per_crawl)):
+        start_day = end_day - delta
+        #ディレクトリを国籍ごとに変える
+        google_crawler = GoogleImageCrawler(downloader_threads=4, storage={'root_dir': './image ' + keyword.split()[0]})
+        google_crawler.crawl(keyword=keyword, filters={'date': (datetime2tuple(start_day), datetime2tuple(end_day))}, file_idx_offset=i*n_per_crawl, max_num=n_per_crawl)
+        end_day = start_day - datetime.timedelta(days=1)
